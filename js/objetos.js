9bf286c67af885a06f979155d49d72ca9bd1dd0c
juanMelgratti <juanmelgratti@mi.unc.edu.ar>
2020-11-16 11:36:34 -0300

    se agregó la funcionalidad de ver el valor total en el carrito

--- js/objetos.js
+++ js/objetos.js
@@ -1,5 +1,3 @@
-var total = 0;
-
 class Producto{
     constructor(imagen, precio, url){
         this.imagen = imagen
@@ -7,28 +5,4 @@ class Producto{
         this.url = url
     }
 };
-
-class Carrito{
-    constructor(){
-        this.carrito = [];
-    }
-
-    agregarProducto(producto){
-        this.carrito.push(producto);
-    }
-
-    verItemsEnCarrito(){
-        this.carrito.foreach(producto =>{
-            return (producto.titulo, producto.precio);
-        })
-}};
-
-let productos = [new Producto('producto1',2,'url'), new Producto('producto2',2,'url'), new Producto('producto3',2,'url')]
-
-let carro = new Carrito()
-
-function botonCarrito(n){
-    carro.agregarProducto(productos[n])
-    console.log(carro.carrito)
-}
 
--- js/productos.js
+++ js/productos.js
@@ -1,11 +1,14 @@
+var carro = []
+
+let carrito = new Carrito();
+let total = 0;
+
 function armarProcesador(serie, modelo){
-    //debugger
     let p = [];
     let prom = 0;
     let urlBusqueda = 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1693&q='+serie;
     for(let i=0; i<6; i++ ){
     return fetch(urlBusqueda)
-
         .then(response => response.json())
         .then(data => {
             var producto = data.results[i];
@@ -19,8 +22,7 @@ function armarProcesador(serie, modelo){
             console.logprom
             return new Producto(p[0].thumbnail, prom, p[0].permalink);
     }
-    )}
-    
+    )}    
 }
 
 function armarProcesadores(){
@@ -50,15 +52,10 @@ function armarProcesadores(){
             </div>   
         `
         document.getElementById("productos").innerHTML = resultados;
-    })} 
-    
-        
+    })}         
 }
 
-
-
 function armarMotherboard(serie, marca){
-    //debugger
     let p = [];
     let prom = 0;
     let urlBusqueda = 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1692&q='+serie;
@@ -77,12 +74,11 @@ function armarMotherboard(serie, marca){
             console.logprom
             return new Producto(p[0].thumbnail, prom, p[0].permalink);
     }
-    )}
-    
+    )}  
 }
 
 function armarMotherboards(){
-    let resultados = ''
+    let resultados = '';
     let mothers=[{'modelo':'Gigabyte', 'serie':'H310'}, {'modelo':'Msi', 'serie':'H310M'}];
     for(let i = 0; i<mothers.length; i++){   
     armarMotherboard(mothers[i]['serie'], mothers[i]['modelo'])
@@ -98,7 +94,7 @@ function armarMotherboards(){
                 <figcaption class="info-wrap">
                   <h6 class="title text-dots"><a href="`+ mother.url +`">`+ mothers[i]['serie'] + mothers[i]['modelo'] +`</a></h6>
                   <div class="action-wrap">
-                    <a href="#" class="btn btn-primary btn-sm float-right" onclick="botonCarrito(0)"> <i class="fas fa-cart-plus"></i> </a>
+                    <a href="#" class="btn btn-primary btn-sm float-right" onclick="agregarACarrito(`+`'`+mothers[i]['serie']+`',`+`'`+mothers[i]['modelo']+`'`+`)"> <i class="fas fa-cart-plus"></i> </a>
                     <div class="price-wrap h5">
                       <span class="price-new">Precio:$` + mother.precio + `</span>
                     </div> 
@@ -115,12 +111,54 @@ function armarMotherboards(){
 function cambiar(n){
     switch(n > 0 ){
         case n == 1:
-            armarMotherboards()
+            armarMotherboards();
             break;
         case n ==2:
-            armarProcesadores()
+            armarProcesadores();
             break;
     }
 }
+
+function agregarACarrito(serie, modelo){
+  armarMotherboard(serie, modelo)
+  .then(producto => {
+    carro.push(producto)
+    total += producto.precio
+    document.getElementById("total").innerHTML = total; 
+  })
+  console.log(carro)
+}
+
+/////////////////////////////////////////////////////////
+// pruebas
+function testProductos(){
+  let prueba = new Producto('url de la imagen', 5000, 'https//:www.google.com.ar');
+  console.log('inicio de prueba');
+  console.log(prueba.imagen);
+  console.log(prueba.precio);
+  console.log(prueba.url);
+  console.log('fin de prueba');
+}
+
+`esta prueba muestra que el objeto funciona.
+al principio tenía un objeto llamado carrito pero dado que
+el fetch de JavaScript es asincrono me estaba dando muchos problemas
+entonces decidí armar un array vacío y llamarlo carrito, ahí se van a guardar
+todos los objetos creados con armarMotherboard, armarProcesadores etc
+e ir pusheando cada objeto por separado guardandolo en el array carrito,
+la idea es guardar el carrito en el local storage para que no se borre.
+Pero eso todavía lo tengo que implementar.` 
+//comentario
+`
+Actualmente hay una sentencia "switch" en la función llamada "cambiar" que en este momento
+solo funciona para las funciones "armarProcesadores" y "armarMotherboards" que lo que hacen es
+armar las tarjetas de productos en el html.
+Recibe los parámetros del array asociativo y con eso hace la búsqueda mediante un fetch
+consultando a la api de mercado libre (esto es lo que hacen las funciones armarProcesadores y
+armarMotherboards).
+`
+
+testProductos();
+
     
 
